58e6c6cb3aedc28e0693b99b6c45e3fdb1b8e8a6
Ilya Savchenko <c4talyst12@yandex.ru>
2024-02-27 15:47:41 +0300

    backend cart is done

--- shop/goods/urls.py
+++ shop/goods/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import shoes, index, shoescart
+from .views import shoes, index, shoescart, view_cart, update_cart, remove_item_from_cart
 
 app_name = 'goods'
 
 urlpatterns = [
     path('', index, name='index'),
     path('shoes/', shoes, name='shoes'),
-    path('shoes/<int:product_id>', shoescart, name="shoe")
+    path('shoes/<int:product_id>/', shoescart, name="shoe"),
+    path('add_to_cart/<int:product_id>/', shoescart, name='add_to_cart'),
+    path('view_cart/', view_cart, name='view_cart'),
+    path('update_cart/', update_cart, name='update_cart'),
+    path('remove_item_from_cart/', remove_item_from_cart, name='remove_item_from_cart'),
+
 ]
--- shop/goods/views.py
+++ shop/goods/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from django.contrib import messages
+
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -48,18 +51,112 @@ def shoes(request):
     return render(request, 'catalog/shoes.html', context=context)
 
 
-def shoescart(requeset, product_id):
+# def shoescart(requeset, product_id):
+#     product = get_object_or_404(Product, id=product_id)
+#     all = Product.objects.all()
+#     ln_products = len(all)
+#     look_at = []
+#     for _ in range(3):
+#         look_at.append(all[random.randint(0, ln_products-1)])
+
+#     context = {
+#         'look_at': look_at,
+#         'product': product
+#     }
+
+#     return render(requeset, 'catalog/shoes_id.html', context=context)
+
+
+def shoescart(request, product_id):
+    if request.method == 'POST':
+        size = request.POST.get('size') 
+        product = get_object_or_404(Product, id=product_id)
+        
+        cart = request.session.get('cart', {})
+        print(cart)
+        cart[product_id] = {
+            'name': product.name,
+            'price': product.price,
+            'size': size,
+            'quantity': cart.get(product_id, {}).get('quantity', 0) + 1
+        }
+        request.session['cart'] = cart
+        
+        messages.success(request, 'Товар успешно добавлен в корзину!')
+        return redirect('goods:shoe', product_id=product_id)
+    
     product = get_object_or_404(Product, id=product_id)
-    all = Product.objects.all()
-    ln_products = len(all)
+    all_products = Product.objects.all()
+    ln_products = len(all_products)
     look_at = []
     for _ in range(3):
-        look_at.append(all[random.randint(0, ln_products-1)])
-
+        look_at.append(all_products[random.randint(0, ln_products-1)])
 
     context = {
         'look_at': look_at,
         'product': product
     }
 
-    return render(requeset, 'catalog/shoes_id.html', context=context)
+    return render(request, 'catalog/shoes_id.html', context=context)
+
+
+# def view_cart(request):
+#     cart = request.session.get('cart', {})
+#     cart_items = []
+
+#     for product_id, item_data in cart.items():
+#         product = Product.objects.get(id=product_id)
+#         item = {
+#             'product': product,
+#             'size': item_data['size'],
+#             'quantity': item_data['quantity'],
+#             'total_price': item_data['quantity'] * item_data['price']
+#         }
+#         cart_items.append(item)
+
+#     return render(request, 'catalog/cart.html', {'cart_items': cart_items})
+
+
+def view_cart(request):
+    cart = request.session.get('cart', {})
+    cart_items = []
+
+    total_price = 0  # Инициализируем переменную для общей стоимости корзины
+
+    for product_id, item_data in cart.items():
+        product = Product.objects.get(id=product_id)
+        item = {
+            'product': product,
+            'size': item_data['size'],
+            'quantity': item_data['quantity'],
+            'total_price': item_data['quantity'] * item_data['price']
+        }
+        cart_items.append(item)
+        total_price += item['total_price']  # Увеличиваем общую стоимость на стоимость текущего товара
+
+    return render(request, 'catalog/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+
+def update_cart(request):
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        quantity = request.POST.get('quantity')
+
+        cart = request.session.get('cart', {})
+        if product_id in cart:
+            cart[product_id]['quantity'] = int(quantity)
+            request.session['cart'] = cart
+
+    return redirect('goods:view_cart')
+
+
+def remove_item_from_cart(request):
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+
+        cart = request.session.get('cart', {})
+        if product_id in cart:
+            del cart[product_id]
+            request.session['cart'] = cart
+
+    return redirect('goods:view_cart')
